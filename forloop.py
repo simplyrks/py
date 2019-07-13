f2a1d71bd498d07e541d239ce7c6dceeab29462a
simplyrks <rvkumarsingh14@gmail.com>
2019-07-13 14:40:00 +0530

    python file

---
+++ forloop.py
@@ -0,0 +1,12 @@
+for number in range(1,5):
+    print ("The current number is ",number)
+
+print("---------------------------")
+
+for number in range(1,7,2):
+    print ("The current number is ",number)
+
+print("---------------------------")
+
+for number in range(5,0,-1):
+    print ("The current number is ",number)
